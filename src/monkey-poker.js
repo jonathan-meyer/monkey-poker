86e1e3692a596842b7bb54db43981813146c74a4
Jonathan Meyer <jon@stej.com>
2020-04-21 06:42:05 -0400

    ...

--- src/monkey-poker.js
+++ src/monkey-poker.js
@@ -180,6 +180,15 @@ app.error((error) => {
   console.error("global", { error });
 });
 
+app.receiver.app.use("/install", (req, res) => {
+  const { headers, query } = req;
+
+  res.json({
+    headers,
+    query,
+  });
+});
+
 app.receiver.app.use((req, res, next) => {
   res.status(404).json({
     error: {
