95f315b062adaf1992df59409fd4b6a5b25a82e4
JONATHAN MEYER <jon@stej.com>
2020-11-22 17:41:50 -0500

    switch to React

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   verbose: true,
   collectCoverage: false,
-  testEnvironment: "node"
+  testEnvironment: "node",
+  coverageReporters: ["json", "text", "html"],
 };
---
+++ src/client/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Root from './containers/Root';
+
+// global styles
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.scss';
+
+ReactDOM.render(<Root />, document.getElementById('app'));
